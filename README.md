fe271cc0002504020a0af19dc73861d0b7527b49
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-15 01:47:29 -0400

    added vscode config

--- README.md
+++ README.md
@@ -1,22 +1,14 @@
-# arch linux dev setup
+# arch linux \ macos dev setup
 
-![](https://imgur.com/3oX66rZ,gif)   
+![](https://imgur.com/3oX66rZ,gif)
 ![](https://imgur.com/INQhcKH.png)
 
-## installation script
-
-```
-curl -o desiredDir/install.sh https://raw.githubusercontent.com/nooneknowspeter/dotfiles/refs/heads/main/install/install.sh
-```
-
-```
-wget -o desiredDir/install.sh https://raw.githubusercontent.com/nooneknowspeter/dotfiles/refs/heads/main/install/install.sh
-
-```
-
-## plugins
-
+- [ghostty](https://ghostty.org/)
+- [gnu stow](https://www.gnu.org/software/stow/)
 - [ohmyposh](https://github.com/jandedobbeleer/oh-my-posh)
+- [neovim](https://www.gnu.org/software/stow/)
 - [tmate](https://github.com/tmate-io/tmate)
 - [tmux](https://github.com/tmux/tmux)
 - [tmux plugin manager](https://github.com/tmux-plugins/tpm?tab=readme-ov-file)
+- [wezterm](https://wezfurlong.org/wezterm/index.html)
+- [zsh](https://www.zsh.org/)
--- scripts/gnu-stow.sh
+++ scripts/gnu-stow.sh
@@ -1,46 +1,51 @@
 # which "$SHELL"
 
 CHECK_AND_INSTALL_GNU_STOW() {
-	CHECK_FOR_STOW=$(pacman -Q | grep stow)
+  CHECK_FOR_STOW=$(pacman -Q | grep stow)
 
-	if [[ ! $CHECK_FOR_STOW == '' ]]; then
-		echo -e "\n GNU Stow is installed \n"
-	else
-		echo -e "\n GNU Stow is not installed \n"
+  if [[ ! $CHECK_FOR_STOW == '' ]]; then
+    echo -e "\n GNU Stow is installed \n"
+  else
+    echo -e "\n GNU Stow is not installed \n"
 
-		sudo pacman -S stow --noconfirm
-	fi
+    sudo pacman -S stow --noconfirm
+  fi
 }
 
 STOW_CONFIGURATION() {
-	stow ghostty
-	stow nvim
-	stow tmux
-	stow wezterm
+  stow ghostty
+
+  stow nvim
+
+  stow tmux
+
+  stow wezterm
+
+  stow vscode
 }
 
 CONFIGURATION() {
-	if [[ $SHELL == /usr/bin/zsh ]]; then
-		echo -e "\n zsh \n"
+  if [[ $SHELL == /usr/bin/zsh ]]; then
+    echo -e "\n zsh \n"
 
-		#!/usr/bin/zsh
+    #!/usr/bin/zsh
 
-		stow zsh
+    stow zsh
 
-		CHECK_AND_INSTALL_GNU_STOW
+    CHECK_AND_INSTALL_GNU_STOW
 
-		STOW_CONFIGURATION
-	else
-		echo -e "\n bash \n"
+    STOW_CONFIGURATION
+  else
+    echo -e "\n bash \n"
 
-		#!/usr/bin/bash
+    #!/usr/bin/bash
 
-		stow bash
+    stow bash
 
-		CHECK_AND_INSTALL_GNU_STOW
+    CHECK_AND_INSTALL_GNU_STOW
 
-		STOW_CONFIGURATION
-	fi
+    STOW_CONFIGURATION
+  fi
 }
 
 echo -e "\n stow or no? y/n \n"
@@ -48,9 +53,9 @@ echo -e "\n stow or no? y/n \n"
 read USER_INPUT
 
 if [[ $USER_INPUT = "y" ]]; then
-	echo -e "\n if you ain't stowing then you ain't going \n"
+  echo -e "\n if you ain't stowing then you ain't going \n"
 
-	CONFIGURATION
+  CONFIGURATION
 else
-	echo -e "\n alright then... \n"
+  echo -e "\n alright then... \n"
 fi
