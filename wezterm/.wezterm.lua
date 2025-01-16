bc46d85b9ac1f74195d8e4b3df8a640763b84dd8
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-16 01:46:43 -0400

    wezterm runs powershell as default shell if detected on windows

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,6 +2,17 @@ local wezterm = require("wezterm")
 
 local config = {}
 
+-- determine operating system
+local windows = "x86_64-pc-windows-msvc"
+-- local linux = "x86_64-unknown-linux-gnu"
+-- local macosSilicon = "aarch64-apple-darwin"
+-- local macosIntel = "x86_64-apple-darwin "
+
+-- run powershell if windows is detected
+if wezterm.target_triple == windows then
+  config.default_prog = { "powershell.exe", "-NoLogo" }
+end
+
 -- graphics and rendering
 -- graphics api
 config.front_end = "OpenGL"
