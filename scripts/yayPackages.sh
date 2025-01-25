f6b00905b3061e04b202b8511ecb75c7bcb3adc8
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-25 04:31:29 -0400

    added installed packages to script

---
+++ scripts/yayPackages.sh
@@ -0,0 +1,30 @@
+sudo pacman -Syyu --noconfirm && sudo pacman -S \
+any2ascii \
+asciigames \
+asciiville-aewan \
+bashdb \
+cbftp \
+docker-scout \
+endoh1 \
+epub-thumbnailer-git \
+fontpreview \
+google-chrome \
+grpcurl \
+jqp \
+lazydocker \
+ltex-ls-plus-bin \
+markdown-oxide-git \
+mongosh-bin \
+netcoredbg \
+python-pynvim-git \
+python311 \
+quarto-cli-bin \
+smassh-bin \
+vscode-js-debug \
+xrdp \
+yay \
+yay-debug \
+zsh-vi-mode \
+  -- noconfirm
+
+sudo pacman -Syyu --noconfirm
