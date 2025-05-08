7f250bf960dcd4e0c741d45c89f26835e11d0b05
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-05-08 08:59:55 -0400

    updated path in powershell script

--- .scripts/win32/remove-symlinks.ps1
+++ .scripts/win32/remove-symlinks.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy RemoteSigned
 rm -Force $env:APPDATA\alacritty
 rm -Force $HOME\.gitconfig
 rm -Force $HOME\.glzr
-rm -Force $env:APPDATA\nvim
+rm -Force $env:LOCALAPPDATA\nvim
 rm -Force $HOME\OneDrive\Documents\WindowsPowerShell
 rm -Force $env:APPDATA\Code\User
 rm -Force $HOME\.config\wezterm
