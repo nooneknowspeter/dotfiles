c9a12745085f81f013fafd37431aac38e40fce8f
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-05-22 08:08:50 -0400

    chore (packages): added paths as variables

--- .scripts/unix/linux/arch/getPackages.sh
+++ .scripts/unix/linux/arch/getPackages.sh
@@ -1,4 +1,10 @@
-pacman -Qqen >$DOTFILES/.scripts/unix/linux/arch/pkglist.txt
-pacman -Qqem >$DOTFILES/.scripts/unix/linux/arch/pkglist_aur.txt
+PKGLIST="$DOTFILES"/.scripts/unix/linux/arch/pkglist.txt
+PKGLIST_AUR="$DOTFILES"/.scripts/unix/linux/arch/pkglist_aur.txt
+UNIX_PACKAGES="$DOTFILES"/.packages/packages_unix.txt
 
-cat $DOTFILES/.scripts/linux/arch/pkglist.txt $DOTFILES/.scripts/linux/arch/pkglist_aur.txt | sort -u >$DOTFILES/.packages/packges_unix.txt
+pacman -Qqen >"$PKGLIST"
+pacman -Qqem >"$PKGLIST_AUR"
+
+cat "$PKGLIST" "$PKGLIST_AUR" | sort -u >"$UNIX_PACKAGES"
+
+cat "$UNIX_PACKAGES"
