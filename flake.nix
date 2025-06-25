aa240fa64f66ec717b8812378646eda38822c0c4
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-25 08:44:24 -0400

    feat (nix): added home-manager config for full nixos setup in flake.nix

--- flake.nix
+++ flake.nix
@@ -88,6 +88,18 @@
           ];
         };
 
+        "nooneknows@nixos" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+          extraSpecialArgs = {
+            inherit inputs;
+          };
+
+          modules = [
+            ./home/home.nix
+          ];
+        };
+
       };
 
     };
