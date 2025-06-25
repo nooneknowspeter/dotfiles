2d710a5e862a7cd02924f4d0eeeee609c6ce573e
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-25 04:00:54 -0400

    nix: fixed ghostty config

--- home/packages/gui-devel/ghostty/ghostty.nix
+++ home/packages/gui-devel/ghostty/ghostty.nix
@@ -8,20 +8,11 @@
 
   programs.ghostty = {
     enable = true;
-    settings = {
-      theme = "UltraDark";
-      font-family = "FiraCode Nerd Font";
-      font-size = 11;
-      background-opacity = 0.8;
-      background-blur = false;
-      confirm-close-surface = false;
-      mouse-hide-while-typing = true;
-      command = "TERM=xterm-256color ${pkgs.bash}/bin/bash";
-      keybind = [
-        "performable:ctrl+c=copy_to_clipboard"
-        "performable:ctrl+v=paste_to_clipboard"
-      ];
-    };
   };
+
+  # home.file.".config/ghostty" = {
+  #   source = ./ghostty;
+  #   recursive = true;
+  # };
 
 }
