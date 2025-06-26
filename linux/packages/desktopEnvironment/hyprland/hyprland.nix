0ebfed1c7daaa08e4e1e0a02025181f945e190ce
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-25 23:58:06 -0400

    feat (nix): updated hyprland nix config
    - enabled wayland

--- linux/packages/desktopEnvironment/hyprland/hyprland.nix
+++ linux/packages/desktopEnvironment/hyprland/hyprland.nix
@@ -15,6 +15,20 @@
     hyprsysteminfo
   ];
 
+  wayland.windowManager.hyprland = {
+    # Whether to enable Hyprland wayland compositor
+    enable = true;
+    # The hyprland package to use
+    package = pkgs.hyprland;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+
+    systemd = {
+      enable = true;
+      enableXdgAutostart = true;
+    };
+  };
+
   home.file.".config/hypr" = {
     source = ./hypr;
     recursive = true;
