c78d56608db36235691ab4b4a3d10669a66a9003
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-25 23:38:42 -0400

    fix (nix): updated vscode nix config

--- home/packages/gui-devel/vscode/vscode.nix
+++ home/packages/gui-devel/vscode/vscode.nix
@@ -16,33 +16,21 @@ in
         aaron-bond.better-comments
         asvetliakov.vscode-neovim
         davidanson.vscode-markdownlint
-        docker.docker
         esbenp.prettier-vscode
-        felixzeller.markdown-oxide
         github.github-vscode-theme
-        gitpod.gitpod-desktop
-        henrytsz.nvim
         ms-azuretools.vscode-docker
         ms-kubernetes-tools.vscode-kubernetes-tools
         ms-python.debugpy
-        ms-python.python
+        ms-python.debugpy
         ms-python.vscode-pylance
         ms-vscode-remote.remote-containers
         ms-vscode-remote.remote-ssh
         ms-vscode-remote.remote-ssh-edit
         ms-vscode-remote.remote-wsl
-        ms-vscode.remote-explorer
-        ms-vscode.remote-server
-        oliverparaskos.fountain-lsp
-        piersdeseilligny.betterfountain
         redhat.vscode-yaml
-        s-nlf-fh.glassit
         streetsidesoftware.code-spell-checker
         sumneko.lua
         tal7aouy.icons
-        tek256.simple-dark
-        vincesalvino.dark-plus-black
-        wayou.vscode-todo-highlight
       ];
     };
   };
