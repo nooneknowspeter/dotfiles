8a4811b5d68367a56c2eb9d08a5ecc65c51bceda
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-27 09:37:35 -0400

    fix (nix): transfered steam configuration to system configuration

--- linux/packages/desktopEnvironment/steam/default.nix
+++ linux/packages/desktopEnvironment/steam/default.nix
@@ -6,21 +6,4 @@
     steam
   ];
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    gamescopeSession = {
-      enable = true;
-    };
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
-    extraPackages = with pkgs; [ gamescope ];
-    protontricks = {
-      enable = true;
-    };
-  };
-
-  programs.gamemode.enable = true;
-
 }
