ff9f3fb9001206b832b200b5dd5987a7c1547cd3
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-26 23:43:47 -0400

    refactor (nix): added misc specific configurations as module

--- linux/hosts/x86-64/peter-legion/configuration.nix
+++ linux/hosts/x86-64/peter-legion/configuration.nix
@@ -14,6 +14,7 @@
       ./modules/environment
       ./modules/graphics
       ./modules/i18n
+      ./modules/misc
     ];
 
 }
---
+++ linux/hosts/x86-64/peter-legion/modules/misc/default.nix
@@ -0,0 +1,7 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  nix.extraOptions = '' experimental-features = nix-command flakes '';
+
+}
