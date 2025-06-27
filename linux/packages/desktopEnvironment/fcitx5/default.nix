73bdcd3264c9c14ea735c753428dfad1b3217c99
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-27 14:40:30 -0400

    refactor (nix): configure fcitx5 through raw config

--- linux/packages/desktopEnvironment/fcitx5/default.nix
+++ linux/packages/desktopEnvironment/fcitx5/default.nix
@@ -3,31 +3,28 @@
 {
 
   home.packages = with pkgs; [
-    # fcitx5-with-addons
+    fcitx5-with-addons
+    fcitx5-gtk
     fcitx5-mozc
+    fcitx5-configtool
   ];
 
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5 = {
-      waylandFrontend = true;
-      addons = with pkgs; [
-        fcitx5-gtk
-        fcitx5-mozc
-        fcitx5-configtool
-      ];
-    };
-  };
+  # i18n.inputMethod = {
+  #   enable = true;
+  #   type = "fcitx5";
+  #   fcitx5 = {
+  #     waylandFrontend = true;
+  #     addons = with pkgs; [
+  #       fcitx5-gtk
+  #       fcitx5-mozc
+  #       fcitx5-configtool
+  #     ];
+  #   };
+  # };
 
-  home.file.".config/fcitx5/config" = {
-    source = ./fcitx5/config;
-    recursive = false;
-  };
-
-  home.file.".config/fcitx5/profile" = {
-    source = ./fcitx5/profile;
-    recursive = false;
+  home.file.".config/fcitx5" = {
+    source = ./fcitx5;
+    recursive = true;
   };
 
   home.sessionVariables = {
