909533b4b9c9cb7a4fa2e8115a25bfdda576d4c5
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-27 03:53:16 -0400

    feat (nix): added fcitx5 configuration

--- linux/packages/desktopEnvironment/fcitx5/fcitx5.nix
+++ linux/packages/desktopEnvironment/fcitx5/fcitx5.nix
@@ -3,21 +3,50 @@
 {
 
   home.packages = with pkgs; [
-    fcitx5
-    fcitx5-configtool
+    # fcitx5-with-addons
     fcitx5-mozc
   ];
 
-  # programs.fcitx5 = {
-  #   enable = true;
-  # };
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-mozc
+        fcitx5-configtool
+      ];
+      settings = {
+        inputMethod = {
+          Group."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "mozc";
+        };
+        globalOptions = {
+          Behaviour = {
+            ActiveByDefault = false;
+          };
 
-  # programs.fcitx5-configtool = {
-  #   enable = true;
-  # };
+          Hotkey = {
+            TriggerKey = "SUPER_SPACE";
+            EnumerateWithTriggerKeys = true;
+          };
+        };
+      };
+    };
+  };
 
-  # programs.fcitx5-mozc = {
-  #   enable = true;
-  # };
+  home.sessionVariables = {
+    INPUT_METHOD = "fcitx5";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+  };
 
 }
