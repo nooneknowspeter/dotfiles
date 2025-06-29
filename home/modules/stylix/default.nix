2ca85b1b1e0a9987ee714c9e06dcf03f2fb4e8bb
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-28 23:38:55 -0400

    feat (nix): updated stylix module

--- home/modules/stylix/default.nix
+++ home/modules/stylix/default.nix
@@ -1,14 +1,21 @@
 { config, pkgs, ...  }:
 {
   stylix = {
+
+    autoEnable = true;
+
     enable = true;
+
     base16Scheme = "${pkgs.base16-schemes}/share/themes/black-metal-dark-funeral.yaml";
+
     polarity = "dark";
+
     cursor = {
       package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
-      size = 24;
+      name = "Bibata-Modern-Classic";
+      size = 18;
     };
+
     fonts = {
       serif = {
         package = pkgs.dejavu_fonts;
@@ -21,6 +28,7 @@
       };
 
       monospace = {
+        package = pkgs.nerd-fonts.fira-code;
         name = "FiraCode Nerd Font";
       };
 
@@ -29,11 +37,28 @@
         name = "Noto Color Emoji";
       };
 
-      # sizes = {
-      #   desktop = 11;
-      #   applications = 11;
-      #   terminal = 11;
-      # };
+      sizes = {
+        applications = 11;
+        desktop = 11;
+        terminal = 11;
+      };
     };
+
+    iconTheme = {
+      enable = true;
+      dark = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    image = pkgs.fetchurl {
+      url = "https://github.com/dharmx/walls/blob/main/manga/a_black_and_white_image_of_a_room.jpeg";
+      hash = "ed5864e529fe79d3c21d5b507e1c0273457094fa";
+    };
+
+    opacity = {
+      desktop = 0.8;
+      popups = 0.8;
+    };
+
   };
 }
