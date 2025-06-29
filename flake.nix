b72ea19726b8d4c821567a36630567aa17f87142
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-29 00:08:07 -0400

    fix (home-manager): pkgs variable for aarch64 darwin

--- flake.nix
+++ flake.nix
@@ -135,7 +135,7 @@
         };
 
         "nooneknows@darwin" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
 
           extraSpecialArgs = {
             inherit inputs;
