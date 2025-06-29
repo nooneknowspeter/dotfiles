c9a8ec8dcf68c205cff62ced616ad8ecaa813ca7
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-28 23:59:32 -0400

    refactor (nixos): seperated programs into modules

--- linux/hosts/x86-64/peter-legion/modules/programs/default.nix
+++ linux/hosts/x86-64/peter-legion/modules/programs/default.nix
@@ -1,24 +1,9 @@
 { config, lib, pkgs, ... }:
 {
 
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  programs.zsh.enable = true;
-
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    gamescopeSession.enable = true;
-  };
-
-  programs.gamemode.enable = true;
-
-  environment.systemPackages = with pkgs; [mangohud protonup-qt lutris bottles heroic];
+  imports = [ 
+    ./steam
+    ./zsh
+  ];
 
 }
---
+++ linux/hosts/x86-64/peter-legion/modules/programs/hyprland/default.nix
@@ -0,0 +1,9 @@
+{ config, lib, pkgs, ... }:
+{
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+}
