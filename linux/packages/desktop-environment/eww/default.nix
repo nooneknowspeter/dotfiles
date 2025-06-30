5ec8637f49eb3bb665258753bda19ebad2fb74e8
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-30 06:22:46 -0400

    feat (eww): added eww config

--- linux/packages/desktop-environment/eww/default.nix
+++ linux/packages/desktop-environment/eww/default.nix
@@ -4,4 +4,9 @@
 
   home.packages = with pkgs; [ eww ];
 
+  home.file.".config/eww" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/dotfiles/linux/packages/desktop-environment/eww/hayyaoe";
+  };
+
 }
