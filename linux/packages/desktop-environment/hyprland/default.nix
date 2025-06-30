0113c3fbeda179588b168d35a1d77a82d64c5d03
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-30 06:32:24 -0400

    feat (hyprland): hyprland nix integration with plugins

--- linux/packages/desktop-environment/hyprland/default.nix
+++ linux/packages/desktop-environment/hyprland/default.nix
@@ -1,11 +1,10 @@
-{ config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
 
   home.packages = with pkgs; [
-    hyprland
-
     hyprcursor
+    hyprgraphics
     hypridle
     hyprland-protocols
     hyprland-qtutils
@@ -16,47 +15,38 @@
     hyprshot
     hyprsunset
     hyprsysteminfo
-
-    hyprlandPlugins.borders-plus-plus
-    hyprlandPlugins.csgo-vulkan-fix
-    hyprlandPlugins.hypr-dynamic-cursors
-    hyprlandPlugins.hyprtrails
-    hyprlandPlugins.hyprwinwrap
-    xdg-desktop-portal-hyprland
+    hyprutils
+    hyprwayland-scanner
+    pyprland
+    wayland-utils
   ];
 
-  # wayland.windowManager.hyprland = {
-  #   # Whether to enable Hyprland wayland compositor
-  #   enable = true;
-  #   # The hyprland package to use
-  #   package = pkgs.hyprland;
-  #   # Whether to enable XWayland
-  #   xwayland.enable = true;
-  #
-  #   plugins = with pkgs; [
-  #     xdg-desktop-portal-hyprland
-  #     hyprlandPlugins.csgo-vulkan-fix
-  #     hyprlandPlugins.borders-plus-plus
-  #     hyprlandPlugins.hypr-dynamic-cursors
-  #     hyprlandPlugins.hyprtrails
-  #     hyprlandPlugins.hyprwinwrap
-  #   ];
-  #
-  #   systemd = {
-  #     enable = true;
-  #     enableXdgAutostart = true;
-  #     variables = ["--all"];
-  #   };
-  #
-  #   extraConfig = ''
-  #
-  #   '';
-  # };
-
-  # services.hyprpaper.enable = true;
-
-  home.file.".config/hypr" = {
-    source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/dotfiles/linux/packages/desktop-environment/hyprland/hypr";
+  wayland.windowManager.hyprland = {
+    enable = true;
+    # set the flake package
+    package =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    plugins = [
+      # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.borders-plus-plus
+      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.csgo-vulkan-fix
+      # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
+      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprexpo
+      # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprfocus
+      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprscrolling
+      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprtrails
+      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprwinwrap
+      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.xtra-dispatchers
+    ];
+    extraConfig = ''
+      # main
+      source = ~/dotfiles/linux/packages/desktop-environment/hyprland/hypr/main.conf
+    '';
   };
+
+  # home.file.".config/hypr" = {
+  #   source = config.lib.file.mkOutOfStoreSymlink
+  #     "${config.home.homeDirectory}/dotfiles/linux/packages/desktop-environment/hyprland/hypr";
+  # };
 }
