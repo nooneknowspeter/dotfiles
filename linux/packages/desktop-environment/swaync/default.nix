e33e7feea80817bd754384a5eb078b40346d5d88
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-30 08:08:57 -0400

    feat (swaync): added config for swaync

--- linux/packages/desktop-environment/swaync/default.nix
+++ linux/packages/desktop-environment/swaync/default.nix
@@ -2,6 +2,11 @@
 
 {
 
-  home.packages = with pkgs; [ libnotify swaynotificationcenter ];
+  home.packages = with pkgs; [ libnotify swaynotificationcenter gvfs ];
+
+  home.file.".config/swaync" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/dotfiles/linux/packages/desktop-environment/swaync/elifouts";
+  };
 
 }
