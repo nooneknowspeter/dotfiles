3607c4da5c7ef86d18bfa9b3ebb4725afa59a4e1
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-01 01:22:07 -0400

    feat (openrgb): added openrgb config

--- linux/packages/desktop-environment/openrgb/default.nix
+++ linux/packages/desktop-environment/openrgb/default.nix
@@ -4,4 +4,9 @@
 
   home.packages = with pkgs; [ openrgb-with-all-plugins ];
 
+  home.file.".config/OpenRGB" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/dotfiles/linux/packages/desktop-environment/openrgb/OpenRGB";
+  };
+
 }
