61a2e5ed0e6eecb3befd8de4a3df489eb4585921
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-03 01:32:10 -0400

    feat (fcitx5): nixify config

--- linux/packages/desktop-environment/fcitx5/default.nix
+++ linux/packages/desktop-environment/fcitx5/default.nix
@@ -3,36 +3,63 @@
 {
 
   home.packages = with pkgs; [
-    fcitx5
+    # fcitx5
     # fcitx5-with-addons
     # fcitx5-gtk
-    fcitx5-mozc
-    fcitx5-configtool
+    # fcitx5-mozc
+    # fcitx5-configtool
+    # fcitx5-anthy
+    # rime-data
+    # fcitx5-rime
   ];
+  
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-mozc
+        fcitx5-anthy
+        fcitx5-configtool
+      ];
+      settings = {
+        globalOptions = {
+          Hotkey = {
+            EnumerateWithTriggerKeys = true;
+            EnumerateSkipFirst = false;
+          };
+          
+          "Hotkey/TriggerKeys" = {
+            "0" = "Super+space";
+          };
+        };
 
-  # i18n.inputMethod = {
-  #   enable = true;
-  #   type = "fcitx5";
-  #   fcitx5 = {
-  #     waylandFrontend = true;
-  #     addons = with pkgs; [
-  #       fcitx5-gtk
-  #       fcitx5-mozc
-  #       fcitx5-configtool
-  #     ];
-  #   };
-  # };
-
-  home.file.".config/fcitx5" = {
-    source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/dotfiles/configs/fcitx5";
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "mozc";
+        };
+      };
+    };
   };
 
+  # home.file.".config/fcitx5" = {
+  #   source = config.lib.file.mkOutOfStoreSymlink
+  #     "${config.home.homeDirectory}/dotfiles/configs/fcitx5";
+  # };
+
   home.sessionVariables = {
-    INPUT_METHOD = "fcitx5";
     GTK_IM_MODULE = "fcitx";
     QT_IM_MODULE = "fcitx";
     XMODIFIERS = "@im=fcitx";
+    GLFW_IM_MODULE = "ibus";
   };
 
 }
