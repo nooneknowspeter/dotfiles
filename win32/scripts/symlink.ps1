e41797083abbb95f8ac8465b08b9aa61d50ce6f5
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-03 07:36:37 -0400

    refactor(windows): update paths to configs in symlink script

--- win32/scripts/symlink.ps1
+++ win32/scripts/symlink.ps1
@@ -1,19 +1,19 @@
 Set-ExecutionPolicy RemoteSigned
 
 # git
-New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\home\packages\headless-essentials\git\.gitconfig" -Path "$HOME\.gitconfig"
+New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\configs\.gitconfig" -Path "$HOME\.gitconfig"
 
 # glazewm
-New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\win32\packages\.glzr" -Path "$HOME\.glzr"
+New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\configs\.glzr" -Path "$HOME\.glzr"
 
 # nvim - minimal setup
-New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\win32\packages\nvim" -Path "$env:LOCALAPPDATA\nvim"
+New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\configs\nvim" -Path "$env:LOCALAPPDATA\nvim"
 
 # powershell
-New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\win32\packages\WindowsPowerShell" -Path "$HOME\OneDrive\Documents\WindowsPowerShell"
+New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\configs\WindowsPowerShell" -Path "$HOME\OneDrive\Documents\WindowsPowerShell"
 
 # vscode
-New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\home\packages\gui-devel\vscode\User" -Path "$env:APPDATA\Code\User"
+New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\configs\Code" -Path "$env:APPDATA\Code"
 
 # wezterm
-New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\home\packages\gui-devel\wezterm\wezterm" -Path "$HOME\.config\wezterm"
+New-Item -ItemType SymbolicLink -Target "$HOME\dotfiles\configs\wezterm" -Path "$HOME\.config\wezterm"
