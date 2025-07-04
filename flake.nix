f240d59063667e09a9659c26aa52b879070bc3b0
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-04 10:47:10 -0400

    fixup! feat(nix): add project based dev shell with packages for development

--- flake.nix
+++ flake.nix
@@ -67,11 +67,9 @@
         inputs.nixpkgs.lib.genAttrs supportedSystems
         (system: f { pkgs = import inputs.nixpkgs { inherit system; }; });
     in {
-
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-      };
             # json
             prettier
 
