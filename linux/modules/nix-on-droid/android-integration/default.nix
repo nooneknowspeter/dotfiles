7a724b2501e94ec49ae1b16444510556a0c7c5b8
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-06 03:30:43 -0400

    refactor (nix-on-droid): seperated nix-on-droid setup into shared modules between hosts

---
+++ linux/modules/nix-on-droid/android-integration/default.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }: {
+
+  android-integration = {
+    termux-open.enable = true;
+    termux-open-url.enable = true;
+    termux-reload-settings.enable = true;
+    termux-wake-lock.enable = true;
+    termux-wake-unlock.enable = true;
+    termux-setup-storage.enable = true;
+    xdg-open.enable = true;
+  };
+
+}
