b0c8bf708c8035ee10cf1323729aaf4a8dbccaf2
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-06 17:31:19 -0400

    refactor(nix): update neovim declaration with additional packages to allow binary download via Mason

--- home/packages/headless-devel/neovim/default.nix
+++ home/packages/headless-devel/neovim/default.nix
@@ -7,8 +7,50 @@
 
   programs.neovim = {
     enable = true;
+    package = pkgs.neovim-unwrapped;
     defaultEditor = true;
-    extraPackages = with pkgs; [ curl git gnutar gzip imagemagick unzip ];
+    extraPackages = with pkgs; [
+      curl
+      git
+      gnutar
+      gzip
+      imagemagick
+      ripgrep
+      unzip
+    ];
+    withNodeJs = true;
+    withPython3 = true;
+    withRuby = true;
+
+    # https://github.com/ryan4yin/nix-config/blob/main/home/base/tui/editors/neovim/default.nix
+    extraWrapperArgs = with pkgs; [
+      "--suffix"
+      "LIBRARY_PATH"
+      ":"
+      "${lib.makeLibraryPath [
+        cargo
+        go
+        openssl
+        pkg-config
+        python314
+        rustc
+        stdenv.cc.cc
+        zlib
+      ]}"
+      "--suffix"
+      "PKG_CONFIG_PATH"
+      ":"
+      "${lib.makeSearchPathOutput "dev" "lib/pkgconfig" [
+        cargo
+        go
+        openssl
+        pkg-config
+        python314
+        rustc
+        stdenv.cc.cc
+        zlib
+      ]}"
+    ];
   };
 
   home.file.".config/nvim" = {
