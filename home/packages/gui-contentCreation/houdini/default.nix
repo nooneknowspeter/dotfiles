ba770e1a7aa7a7778009a58777563485b872aa03
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-10 01:20:00 -0400

    refactor (nix): updated houdini package

--- home/packages/gui-contentCreation/houdini/default.nix
+++ home/packages/gui-contentCreation/houdini/default.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ... }: {
 
+  # https://github.com/NixOS/nixpkgs/issues/251551
   home.packages = with pkgs; [ houdini ];
-
-  # programs.houdini = {
-  #   enable = true;
-  # };
 
 }
