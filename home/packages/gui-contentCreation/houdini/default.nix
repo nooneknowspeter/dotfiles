ab27db96b318ff495fd41ab31e394977eb63f488
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-13 10:36:25 -0400

    fix(home-manager): launch houdini with shell bin script

--- home/packages/gui-contentCreation/houdini/default.nix
+++ home/packages/gui-contentCreation/houdini/default.nix
@@ -1,6 +1,27 @@
 { config, pkgs, ... }: {
 
   # https://github.com/NixOS/nixpkgs/issues/251551
-  home.packages = with pkgs; [ houdini ];
+  home.packages = with pkgs;
+    [
+      # houdini
+      (
+        let
+          inherit (config.home) username;
+          inherit (lib) getExe;
+          houdiniExe = getExe houdini;
+          sesinetdExe = "${houdini}/houdini/sbin/sesinetd";
+          licenseDir = "${config.xdg.dataHome}/houdini/licenses";
+        in
+        writeShellApplication {
+          name = "houdini";
+          runtimeInputs = [ ];
+          text = ''
+            mkdir -p ${licenseDir}
+            ${sesinetdExe} ${licenseDir} --user ${username} --group ${username}
+            ${houdiniExe}
+          '';
+        }
+      )
+    ];
 
 }
