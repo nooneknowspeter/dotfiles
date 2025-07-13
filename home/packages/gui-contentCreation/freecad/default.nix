e06715cc2f5584a2041d300a99c3d819ef5a70d2
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-13 10:34:21 -0400

    fix (home-manager): launch freecad with shell bin script using x11

--- home/packages/gui-contentCreation/freecad/default.nix
+++ home/packages/gui-contentCreation/freecad/default.nix
@@ -1,5 +1,12 @@
 { config, pkgs, ... }: {
 
-  home.packages = with pkgs; [ freecad ];
+  # https://github.com/FreeCAD/FreeCAD/issues/17491
+  home.packages = with pkgs;
+    [
+      # freecad
+      (writeShellScriptBin "freecad" ''
+        QT_QPA_PLATFORM=xcb ${freecad}/bin/freecad
+      '')
+    ];
 
 }
