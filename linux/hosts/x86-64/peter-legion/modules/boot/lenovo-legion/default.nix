be3af2afa1317d303dd7b9b07b5d8820a31c2104
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-18 13:21:51 -0400

    fix(nixos): add lenovo legion modules to peter-legion

---
+++ linux/hosts/x86-64/peter-legion/modules/boot/lenovo-legion/default.nix
@@ -0,0 +1,8 @@
+{ config, lib, pkgs, ... }: {
+  # https://discourse.nixos.org/t/how-do-i-control-my-laptops-fan-speeds/45964/4
+
+  boot.kernelModules = [ "lenovo-legion-module" ];
+  boot.extraModulePackages = with config.boot.kernelPackages;
+    [ lenovo-legion-module ];
+
+}
--- linux/hosts/x86-64/peter-legion/packages/lenovo-legion/default.nix
+++ linux/hosts/x86-64/peter-legion/packages/lenovo-legion/default.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }: {
 
-  environment.systemPackages = with pkgs; [ dmidecode lenovo-legion mokutil ];
+  environment.systemPackages = with pkgs; [ lenovo-legion ];
 
 }
