f4b86034cf6d7bd9931e444ad4802c2e24027495
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-24 16:02:56 -0400

    feat(nixos): add additional kernal modules to enable drm and fbdev for nvidia hardware

--- linux/modules/nixos/hardware/nvidia/default.nix
+++ linux/modules/nixos/hardware/nvidia/default.nix
@@ -1,4 +1,9 @@
-{ ... }:
-{
+{ ... }: {
   hardware.nvidia.modesetting.enable = true;
+
+  boot = {
+    initrd.kernelModules =
+      [ "nvidia" "i915" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+    kernelParams = [ "nvidia-drm.fbdev=1" "nvidia-drm.modeset=1" ];
+  };
 }
