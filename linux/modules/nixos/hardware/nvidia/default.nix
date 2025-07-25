5a7352e66a4cbc6613c5450079c2235e64ab9d40
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-25 00:22:33 -0400

    refactor(nixos): remove nvidia kernel params and use hardware.nvidia attribute set

--- linux/modules/nixos/hardware/nvidia/default.nix
+++ linux/modules/nixos/hardware/nvidia/default.nix
@@ -1,9 +1,8 @@
 { ... }: {
-  hardware.nvidia.modesetting.enable = true;
 
-  boot = {
-    initrd.kernelModules =
-      [ "nvidia" "i915" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
-    kernelParams = [ "nvidia-drm.fbdev=1" "nvidia-drm.modeset=1" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
   };
+
 }
