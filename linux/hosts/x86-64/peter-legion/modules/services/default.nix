fd3c80e2c05d59d3f276ac4400c517021ee04bf7
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-24 23:06:06 -0400

    refactor(nixos/peter-legion): remove service/kmscon module

--- linux/hosts/x86-64/peter-legion/modules/services/default.nix
+++ linux/hosts/x86-64/peter-legion/modules/services/default.nix
@@ -6,7 +6,6 @@
 
     # shared modules
     ../../../../../modules/nixos/services/greetd
-    ../../../../../modules/nixos/services/kmscon
     ../../../../../modules/nixos/services/libinput
     ../../../../../modules/nixos/services/logind
     ../../../../../modules/nixos/services/openrgb
