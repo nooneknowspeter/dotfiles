3a630478268b772f22f687b0dc1b35226342af49
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-24 23:41:52 -0400

    feat(nixos): add black metal dark funeral colorscheme to linux tty console

--- linux/modules/nixos/console/default.nix
+++ linux/modules/nixos/console/default.nix
@@ -1,10 +1,30 @@
-{ ... }:
-{
+{ ... }: {
 
   console = {
     font = "972";
     # keyMap = "us";
     # useXkbConfig = true; # use xkb.options in tty.
+    colors = [
+      "000000"
+      "c1c1c1"
+      "c1c1c1"
+      "000000"
+      "5f8787"
+      "d0dfee"
+      "5f81a5"
+      "888888"
+      "999999"
+      "aaaaaa"
+      "c1c1c1"
+      "333333"
+      "5f8787"
+      "d0dfee"
+      "5f81a5"
+      "888888"
+      "999999"
+      "aaaaaa"
+      "c1c1c1"
+    ];
   };
 
 }
