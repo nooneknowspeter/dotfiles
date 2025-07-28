f171c7fe3125f24e792a6cced05da42184dc4e6b
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-27 20:44:48 -0400

    refactor(home-manager/vscode): set symlinks to independent files in config

--- home/packages/gui-devel/vscode/default.nix
+++ home/packages/gui-devel/vscode/default.nix
@@ -1,39 +1,21 @@
 { config, pkgs, ... }:
 let
-  configLocation = if pkgs.stdenv.isLinux then ".config/Code" else "Library/Application Support/Code";
-in
-{
+  configLocation = if pkgs.stdenv.isLinux then
+    ".config/Code"
+  else
+    "Library/Application Support/Code";
+in {
 
-  home.packages = with pkgs; [ vscode-with-extensions ];
+  home.packages = with pkgs; [ vscode-fhs ];
 
-  programs.vscode = {
-    enable = true;
-    profiles."nooneknows" = {
-      extensions = with pkgs.vscode-extensions; [
-        aaron-bond.better-comments
-        asvetliakov.vscode-neovim
-        davidanson.vscode-markdownlint
-        esbenp.prettier-vscode
-        github.github-vscode-theme
-        ms-azuretools.vscode-docker
-        ms-kubernetes-tools.vscode-kubernetes-tools
-        ms-python.debugpy
-        ms-python.debugpy
-        ms-python.vscode-pylance
-        ms-vscode-remote.remote-containers
-        ms-vscode-remote.remote-ssh
-        ms-vscode-remote.remote-ssh-edit
-        ms-vscode-remote.remote-wsl
-        redhat.vscode-yaml
-        streetsidesoftware.code-spell-checker
-        sumneko.lua
-        tal7aouy.icons
-      ];
-    };
+  home.file."${configLocation}/User/settings.json" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/dotfiles/configs/Code/User/settings.json";
   };
 
-  home.file."${configLocation}" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/configs/Code";
+  home.file."${configLocation}/User/keybindings.json" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/dotfiles/configs/Code/User/keybindings.json";
   };
 
 }
