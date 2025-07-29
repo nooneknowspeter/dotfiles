60853e0242716aa232121a16348424fd930e8801
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-28 20:27:07 -0400

    feat(home-manager): add automatic garbage collection attribute to nix

--- home/modules/nix/default.nix
+++ home/modules/nix/default.nix
@@ -1,4 +1,13 @@
+{ pkgs, ... }: {
+
   nix = {
     package = pkgs.nix;
     settings.experimental-features = [ "nix-command" "flakes" ];
+    gc = {
+      automatic = true;
+      frequency = "weekly";
+      options = "--delete-older-than 14d";
+    };
   };
+
+}
