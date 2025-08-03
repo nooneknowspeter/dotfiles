e02ab33d766d3cbd1724832b597812bcb61d486c
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-02 21:06:43 -0400

    feat(nixos/networking): add networkmanager vpn plugins

--- linux/modules/nixos/networking/default.nix
+++ linux/modules/nixos/networking/default.nix
@@ -1,10 +1,22 @@
-{ hostname, ... }:
+{ hostname, pkgs, ... }:
 {
 
   # hostname
   networking.hostName = hostname;
 
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-fortisslvpn
+      networkmanager-iodine
+      networkmanager-l2tp
+      networkmanager-openconnect
+      networkmanager-openvpn
+      networkmanager-sstp
+      networkmanager-strongswan
+      networkmanager-vpnc
+    ];
+  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
