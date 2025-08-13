c26b43941f7b4d5171567324435b716402b25527
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-13 18:14:41 -0400

    fix(home-manager/packages/freecad): update freecad package definition

--- home/packages/gui-engineering/freecad/default.nix
+++ home/packages/gui-engineering/freecad/default.nix
@@ -2,11 +2,6 @@
 {
 
   # https://github.com/FreeCAD/FreeCAD/issues/17491
-  home.packages = with pkgs; [
-    # freecad
-    (writeShellScriptBin "freecad" ''
-      QT_QPA_PLATFORM=xcb ${freecad}/bin/freecad
-    '')
-  ];
+  home.packages = with pkgs; [ freecad ];
 
 }
