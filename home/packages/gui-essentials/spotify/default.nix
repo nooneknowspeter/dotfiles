f8a08230dd4859119bdd013efa05f41909cc9a9d
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-18 21:33:49 -0400

    feat(home-manager/packages/spotify): add spicetify configuration

---
+++ home/packages/gui-essentials/spotify/default.nix
@@ -0,0 +1,24 @@
+{
+  inputs,
+  config,
+	lib,
+  pkgs,
+  ...
+}:
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+in
+{
+	imports = [
+  inputs.spicetify-nix.homeManagerModules.spicetify 
+];
+	
+  programs.spicetify = {
+    enable = true;
+    enabledExtensions = with spicePkgs.extensions; [
+      adblockify
+      hidePodcasts
+      shuffle # shuffle+ (special characters are sanitized out of extension names)
+    ];
+  };
+}
