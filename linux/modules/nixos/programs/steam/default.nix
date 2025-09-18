22b409e1cdaaba54df55299f3eaa876e61ee39f4
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-18 05:53:40 -0400

    refactor(nixos/packages/steam): add protontricks

--- linux/modules/nixos/programs/steam/default.nix
+++ linux/modules/nixos/programs/steam/default.nix
@@ -7,6 +7,7 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     gamescopeSession.enable = true;
+    protontricks.enable = true;
   };
 
   programs.gamemode.enable = true;
