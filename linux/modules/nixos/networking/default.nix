635f25918a20a7fc68009100d06165791c9f4a9e
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-18 03:57:12 -0400

    refactor(nixos): remove network manager plugins to vpn module

--- linux/modules/nixos/networking/default.nix
+++ linux/modules/nixos/networking/default.nix
@@ -6,16 +6,6 @@
 
   networking.networkmanager = {
     enable = true;
-    plugins = with pkgs; [
-      networkmanager-fortisslvpn
-      networkmanager-iodine
-      networkmanager-l2tp
-      networkmanager-openconnect
-      networkmanager-openvpn
-      networkmanager-sstp
-      networkmanager-strongswan
-      networkmanager-vpnc
-    ];
   };
 
   # Configure network proxy if necessary
