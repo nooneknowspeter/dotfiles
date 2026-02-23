e3374b19cc5aab80c8937f63ef743774d3bd65a9
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-02-23 14:50:53 -0400

    fix(home/package/gui-engineering/bambu-studio): update package

--- home/packages/gui-engineering/bambu-studio/default.nix
+++ home/packages/gui-engineering/bambu-studio/default.nix
@@ -1,6 +1,18 @@
 { config, pkgs, ... }:
+
+let
+  pkgsNoCuda = import pkgs.path {
+    inherit (pkgs.stdenv) system;
+    config = {
+      allowUnfree = true;
+      cudaSupport = false;
+    };
+  };
+in
 {
 
-  home.packages = with pkgs; [ bambu-studio ];
+  home.packages = with pkgsNoCuda; [
+    bambu-studio
+  ];
 
 }
