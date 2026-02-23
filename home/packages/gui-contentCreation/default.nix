41e9455188d0fec590e1d4f84c3db3c4226a0a2c
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-02-23 12:16:11 -0400

    refactor(home/packages/gui-contentCreation): remove ardour and audio plugins

--- home/packages/gui-contentCreation/default.nix
+++ home/packages/gui-contentCreation/default.nix
@@ -3,8 +3,6 @@
   imports = import ../../../lib/iterate-dirs.nix {
     directory = ./.;
     blacklistArray = [
-      "ardour"
-      "audio-plugins"
       "blender"
       "houdini"
       "natron"
