4c102b0597308ac14a7b30c4935584b510d09938
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-03-01 11:48:57 -0400

    fix(linux/modules/nixos/misc/vnc): update udev rules

--- linux/modules/nixos/misc/vnc/default.nix
+++ linux/modules/nixos/misc/vnc/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
 
   services.sunshine = {
@@ -6,6 +6,15 @@
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
+
+    package = pkgs.sunshine.override {
+      cudaSupport = true;
+      cudaPackages = pkgs.cudaPackages;
+    };
   };
+
+  services.udev.extraRules = ''
+    		KERNEL=="uinput", MODE="0660", GROUP="input", SYMLINK+="uinput"
+    	'';
 
 }
