64cf7d156dcfbc0faa1be14ab523bd6e78a3c4aa
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-04-16 11:23:14 -0400

    feat(configs/yazi): add settings.toml

--- home/packages/headless-essentials/yazi/default.nix
+++ home/packages/headless-essentials/yazi/default.nix
@@ -1,10 +1,23 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
 
-  home.packages = with pkgs; [ yazi ];
+  home.packages = with pkgs; [
+    yazi
+    exiftool
+  ];
 
   programs.yazi = {
     enable = true;
+    settings = {
+      yazi = lib.importTOML ../../../../configs/yazi/settings.toml;
+      # keymap = lib.importTOML ./keymap.toml;
+      #theme = lib.importTOML ./theme.toml;
+    };
   };
 
 }
