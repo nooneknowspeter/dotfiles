38ec340ee69ef23c0d0e3eb271f2407029275e40
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-04-29 21:27:18 -0400

    refactor(home/packages/gui-devel/ghostty): use ghostty flake package

--- home/packages/gui-devel/ghostty/default.nix
+++ home/packages/gui-devel/ghostty/default.nix
@@ -6,15 +6,15 @@
 }:
 {
 
-  home.packages = with pkgs; [
-    ghostty
-  ];
+  # home.packages = with pkgs; [
+  #   ghostty
+  # ];
 
-  # programs.ghostty = {
-  #   enable = true;
-  #   package = inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.ghostty-releasefast;
-  #   enableZshIntegration = false;
-  # };
+  programs.ghostty = {
+    enable = true;
+    package = inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.ghostty-releasefast;
+    enableZshIntegration = false;
+  };
 
   home.file.".config/ghostty" = {
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/configs/ghostty";
