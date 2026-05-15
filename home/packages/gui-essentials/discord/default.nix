2d573f88404b976ef7b2c931bc4012f1cd65b339
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-05-15 12:44:28 -0400

    refactor(home/packages/gui-essentials/discord): update package

--- home/packages/gui-essentials/discord/default.nix
+++ home/packages/gui-essentials/discord/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 {
-
-  home.packages = with pkgs; [ discord ];
-
+  programs.discord = {
+    enable = true;
+    settings = {
+      DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
+      SKIP_HOST_UPDATE = true;
+    };
+  };
 }
