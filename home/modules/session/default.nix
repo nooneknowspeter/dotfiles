c91bc5f3b56afe5ccfee9a52494dacf94e35e39b
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-06-20 10:47:00 -0400

    refactor(home): update session module
    - add zsh init multi string config; centralization

--- home/modules/session/default.nix
+++ home/modules/session/default.nix
@@ -9,10 +9,59 @@
     RESOURCES = "$HOME/resources";
     SECRETS = "$HOME/secrets";
     PASSWORD_STORE_DIR = "$SECRETS";
+    DOCS = "$HOME/docs";
+    NOTES = "$HOME/notes";
   };
 
   shellAliases = {
     EDITOR = "nvim";
     BROWSER = "brave";
   };
+
+  zshConfig =
+    { pkgs }:
+    ''
+      # fzf-tab
+      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+
+      # bind ctrl + space for autosuggest accept
+      bindkey '^ ' autosuggest-accept
+
+      # cbonsai
+      alias cbonsai="cbonsai -li"
+
+      # cmatrix
+      alias unimatrix="unimatrix -nb -s 96 -l k"
+
+      # clear tmux histroy and clear terminal
+      alias clear="clear; tmux clear-history; clear"
+
+      # ascii.live animations
+      alias donut="curl ascii.live/donut" # donut animation
+      alias knot="curl ascii.live/knot" # knot animation
+
+      # force launch tmux with 256 colors
+      alias tmux="TERM=xterm-256color tmux"
+
+      # ls
+      alias ls="eza"
+
+      # cat
+      alias cat="bat --theme=base16-256"
+
+      # grep
+      alias grep="rg --color=auto"
+
+      # find
+      alias find="fd"
+
+      # lsusb
+      alias lsusb="cyme"
+
+      #du
+      alias du="dust"
+
+      #df
+      alias df="duf -theme 'ansi'"
+    '';
 }
--- home/packages/headless-essentials/zsh/default.nix
+++ home/packages/headless-essentials/zsh/default.nix
@@ -21,50 +21,7 @@ in
     syntaxHighlighting.enable = true;
     sessionVariables = session.sessionVariables;
     shellAliases = session.shellAliases;
-    initContent = ''
-      # fzf-tab
-      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
-
-      # bind ctrl + space for autosuggest accept
-      bindkey '^ ' autosuggest-accept
-
-      # cbonsai
-      alias cbonsai="cbonsai -li"
-
-      # cmatrix
-      alias unimatrix="unimatrix -nb -s 96 -l k"
-
-      # clear tmux histroy and clear terminal
-      alias clear="clear; tmux clear-history; clear"
-
-      # ascii.live animations
-      alias donut="curl ascii.live/donut" # donut animation
-      alias knot="curl ascii.live/knot" # knot animation
-
-      # force launch tmux with 256 colors
-      alias tmux="TERM=xterm-256color tmux"
-
-      # ls
-      alias ls="eza"
-
-      # cat
-      alias cat="bat --theme=base16-256"
-
-      # grep
-      alias grep="rg --color=auto"
-
-      # find
-      alias find="fd"
-
-      # lsusb
-      alias lsusb="cyme"
-
-      #du
-      alias du="dust"
-
-      #df
-      alias df="duf -theme 'ansi'"
-    '';
+    initContent = session.zshConfig { inherit pkgs; };
     oh-my-zsh = {
       enable = true;
       theme = "robbyrussell";
