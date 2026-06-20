91ae118bec1ce7e614d9dcef08153f87b1ba6a0e
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-06-20 10:47:37 -0400

    refactor(home/packages): use environment variables for paths

--- home/packages/headless-devel/atac/default.nix
+++ home/packages/headless-devel/atac/default.nix
@@ -3,6 +3,6 @@
   home.packages = with pkgs; [ atac ];
 
   home.sessionVariables = {
-    ATAC_KEY_BINDINGS = "/home/nooneknows/dotfiles/configs/atac/keybindings.toml";
+    ATAC_KEY_BINDINGS = "$HOME/dotfiles/configs/atac/keybindings.toml";
   };
 }
