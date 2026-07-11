f8e485f1b0269b706a44fd7576d1f8a942945018
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-07-10 20:31:55 -0400

    feat: add photogimp config
    - add photogimp as submodule
    - symlink config

--- home/packages/gui-contentCreation/gimp/default.nix
+++ home/packages/gui-contentCreation/gimp/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home.packages = with pkgs; [ gimp ];
+
+  home.file.".config/GIMP/3.0" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/configs/gimp/photogimp/.config/GIMP/3.0";
+  };
+
 }
